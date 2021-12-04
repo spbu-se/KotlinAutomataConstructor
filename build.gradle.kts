6b96d1ce748e144fefee7e22d136fefaed3364c2
IlyaMuravjov <muravjovilya@gmail.com>
2021-12-05 00:13:37 +0300

    Update to Kotlin 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.6.0"
     application
 }
 version = "1.0-SNAPSHOT"
--- src/main/kotlin/automaton/constructor/model/factory/AbstractAutomatonFactory.kt
+++ src/main/kotlin/automaton/constructor/model/factory/AbstractAutomatonFactory.kt
@@ -4,6 +4,7 @@ import automaton.constructor.model.Automaton
 import automaton.constructor.model.memory.MemoryUnitDescriptor
 import automaton.constructor.utils.Setting
 import automaton.constructor.utils.SettingListEditor
+import java.util.*
 
 abstract class AbstractAutomatonFactory(override val displayName: String) : AutomatonFactory {
     abstract fun createMemoryDescriptors(): List<MemoryUnitDescriptor>
@@ -12,5 +13,5 @@ abstract class AbstractAutomatonFactory(override val displayName: String) : Auto
     override fun createAutomaton() = Automaton(displayName, createMemoryDescriptors())
     override fun createEditor() = createSettings().takeIf { it.isNotEmpty() }?.let { SettingListEditor(it) }
 
-    override fun toString() = displayName.capitalize()
+    override fun toString() = displayName.replaceFirstChar { it.titlecase(Locale.getDefault()) }
 }
