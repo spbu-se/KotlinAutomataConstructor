10ef27ef16669d1ce175081c3b5ae718424d462b
Azim Muradov <azim.muradov.dev@gmail.com>
2022-07-12 21:17:53 +0300

    Change modules' factories visibility to `private`

--- src/main/kotlin/automaton/constructor/model/module/FinalStateWithTransitionDetector.kt
+++ src/main/kotlin/automaton/constructor/model/module/FinalStateWithTransitionDetector.kt
@@ -4,7 +4,7 @@ import automaton.constructor.model.automaton.Automaton
 import automaton.constructor.utils.filteredSet
 import javafx.beans.binding.Bindings.isNotEmpty
 
-val finalStateWithTransitionDetectorFactory = { automaton: Automaton -> FinalStateWithTransitionDetector(automaton) }
+private val finalStateWithTransitionDetectorFactory = { automaton: Automaton -> FinalStateWithTransitionDetector(automaton) }
 val Automaton.finalStatesWithTransitions get() = getModule(finalStateWithTransitionDetectorFactory).finalStatesWithTransitions
 
 class FinalStateWithTransitionDetector(val automaton: Automaton) : AutomatonModule {
--- src/main/kotlin/automaton/constructor/model/module/InitialStateDetector.kt
+++ src/main/kotlin/automaton/constructor/model/module/InitialStateDetector.kt
@@ -3,7 +3,7 @@ package automaton.constructor.model.module
 import automaton.constructor.model.automaton.Automaton
 import automaton.constructor.utils.filteredSet
 
-val initialStateDetectorFactory = { automaton: Automaton -> InitialStateDetector(automaton) }
+private val initialStateDetectorFactory = { automaton: Automaton -> InitialStateDetector(automaton) }
 val Automaton.initialStates get() = getModule(initialStateDetectorFactory).initialStates
 
 class InitialStateDetector(automaton: Automaton) : AutomatonModule {
