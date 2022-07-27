00cbe6c92170d003c64b5dbef3f8af7a97052c7a
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-07-28 00:46:51 +0500

    fix test problem: add three properties in AbstractAutomaton.kt

--- src/main/kotlin/automaton/constructor/model/automaton/AbstractAutomaton.kt
+++ src/main/kotlin/automaton/constructor/model/automaton/AbstractAutomaton.kt
@@ -11,7 +11,9 @@ import automaton.constructor.model.property.EPSILON_VALUE
 import automaton.constructor.model.transition.Transition
 import automaton.constructor.model.transition.storage.TransitionStorage
 import automaton.constructor.model.transition.storage.createTransitionStorageTree
+import automaton.constructor.utils.I18N
 import automaton.constructor.utils.UndoRedoManager
+import automaton.constructor.utils.I18N.messages
 import javafx.collections.ObservableSet
 import javafx.geometry.Point2D
 import tornadofx.*
@@ -36,6 +38,13 @@ abstract class AbstractAutomaton(
     private val incomingTransitions = mutableMapOf<State, MutableSet<Transition>>()
 
     override val undoRedoManager = UndoRedoManager()
+
+    override val deterministicDisplayName: String =
+        I18N.messages.getString("AutomatonDescriptionProvider.FiniteAutomaton.Deterministic")
+    override val nondeterministicDisplayName: String =
+        I18N.messages.getString("AutomatonDescriptionProvider.FiniteAutomaton.Nondeterministic")
+    override val untitledDisplayName: String =
+        I18N.messages.getString("OpenedAutomatonController.UntitledFiniteAutomaton")
 
     init {
         val memoryNameToCountMap = mutableMapOf<String, Int>()
