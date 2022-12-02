d61630efb1463a409aecaa07d53a7f81cbeccbfa
IlyaMuravjov <muravjovilya@gmail.com>
2022-12-02 17:54:44 +0300

    Add startup hint

--- src/main/kotlin/automaton/constructor/AutomatonConstructorApp.kt
+++ src/main/kotlin/automaton/constructor/AutomatonConstructorApp.kt
@@ -1,6 +1,9 @@
 package automaton.constructor
 
+import automaton.constructor.controller.LocaleController
+import automaton.constructor.utils.I18N
 import automaton.constructor.view.MainWindow
+import javafx.scene.control.*
 import javafx.stage.Stage
 import tornadofx.*
 
@@ -9,6 +12,18 @@ class AutomatonConstructorApp : App() {
         FX.stylesheets.add("style.css")
         super.start(stage)
         MainWindow().show()
+        if (config.boolean("showStartUpHint", true))
+            Alert(Alert.AlertType.INFORMATION).apply {
+                title = I18N.messages.getString("Dialog.information")
+                headerText = I18N.messages.getString("Hint.UseRightClickToAddElements")
+                dialogPane.expandableContent = CheckBox(I18N.messages.getString("Hint.DontShowAgain")).apply {
+                    setOnAction {
+                        config["showStartUpHint"] = !isSelected
+                        config.save()
+                    }
+                }
+                dialogPane.isExpanded = true
+            }.show()
     }
 }
 
