696ca4e0ff38c5f98c82c0251e9147aebba3a20f
IlyaMuravjov <muravjovilya@gmail.com>
2023-02-12 19:26:25 +0300

    Support opening file specified by command args

--- src/main/kotlin/automaton/constructor/AutomatonConstructorApp.kt
+++ src/main/kotlin/automaton/constructor/AutomatonConstructorApp.kt
@@ -3,12 +3,14 @@ package automaton.constructor
 import automaton.constructor.controller.LocaleController
 import automaton.constructor.utils.I18N
 import automaton.constructor.view.MainWindow
-import javafx.scene.control.*
+import javafx.scene.control.Alert
+import javafx.scene.control.CheckBox
 import javafx.stage.Stage
 import tornadofx.*
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.text.MessageFormat
 
 class AutomatonConstructorApp : App() {
     override fun start(stage: Stage) {
@@ -19,8 +21,16 @@ class AutomatonConstructorApp : App() {
         MainWindow().apply {
             show()
             if (unnamedParams.isNotEmpty()) {
-                val file = File(unnamedParams[0])
+                val path = unnamedParams.joinToString("")
+                val file = File(path)
                 if (file.exists()) fileController.open(file)
+                else runLater {
+                    error(
+                        title=I18N.messages.getString("Dialog.error"),
+                        header=MessageFormat.format(I18N.messages.getString("Error.FailedToOpenFile.Header"), path),
+                        content=I18N.messages.getString("Error.FailedToOpenFile.Content")
+                    )
+                }
             }
         }
 
