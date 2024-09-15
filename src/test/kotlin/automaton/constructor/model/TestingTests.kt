df2dfc58b5f3a591fe89ab2e83c327f39cb0c70f
IgorFilimonov <71323756+IgorFilimonov@users.noreply.github.com>
2024-09-15 17:21:27 +0300

    UI tests fix (#17)
    * Initial commit
    
    * Trying to fix tests
    
    * Updated to v4 of the actions to upload and download artifacts
    
    * Fix compilation after rebase
    
    ---------
    
    Co-authored-by: IlyaMuravjov <muravjovilya@gmail.com>

--- src/test/kotlin/automaton/constructor/model/TestingTests.kt
+++ src/test/kotlin/automaton/constructor/model/TestingTests.kt
@@ -2,17 +2,18 @@ package automaton.constructor.model
 
 import automaton.constructor.AutomatonConstructorApp
 import automaton.constructor.utils.I18N
+import automaton.constructor.view.TestAndResult
 import javafx.application.Application
 import javafx.scene.Node
+import javafx.scene.control.TableView
 import javafx.stage.Stage
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assumptions.assumeFalse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.testfx.api.FxAssert.verifyThat
 import org.testfx.api.FxToolkit
 import org.testfx.framework.junit5.ApplicationTest
-import org.testfx.matcher.control.TableViewMatchers.hasTableCell
 
 class TestingTests: ApplicationTest() {
     private lateinit var app: Application
@@ -30,10 +31,12 @@ class TestingTests: ApplicationTest() {
 
     @AfterEach
     fun afterEachTest() {
-        FxToolkit.cleanupApplication(app)
+        if (::app.isInitialized) {
+            FxToolkit.cleanupApplication(app)
+        }
     }
 
-    private fun openExampleAndRunTest(automaton: String, test: String) {
+    private fun openExampleAndRunTest(automaton: String, test: String): String {
         clickOn(I18N.messages.getString("OpenedAutomatonController.OK"))
         clickOn(I18N.messages.getString("MainView.File"))
         clickOn(I18N.messages.getString("MainView.Examples"))
@@ -41,57 +44,65 @@ class TestingTests: ApplicationTest() {
         clickOn(I18N.messages.getString("MainView.Tests"))
         clickOn(I18N.messages.getString("MainView.Tests.Create"))
         clickOn(I18N.messages.getString("TestsFragment.Add"))
-        clickOn(from(rootNode(window("Test").scene)).lookup(".text-field").query<Node>()).write(test)
-        clickOn(from(rootNode(window("Test").scene)).lookup(I18N.messages.getString(
-            "TestsFragment.Run")).query<Node>())
-        clickOn(rootNode(window("Tests results").scene))
+        clickOn(
+            from(rootNode(window(I18N.messages.getString("TestsFragment.Title")).scene)
+            ).lookup(".text-field").query<Node>()).write(test)
+        clickOn(
+            from(rootNode(window(I18N.messages.getString("TestsFragment.Title")).scene)
+            ).lookup(I18N.messages.getString("TestsFragment.Run")).query<Node>())
+        val resultsRootNode = rootNode(window(I18N.messages.getString("TestsResultsFragment.Title")).scene)
+        clickOn(resultsRootNode)
+        return from(resultsRootNode).lookup(".table-view").queryAll<TableView<TestAndResult>>().first().items.first().result
     }
 
     @Test
     fun runningTestOnFiniteAutomatonTest() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.evenBinaryNumbersRecognizer"), "1")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Rejected")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.evenBinaryNumbersRecognizer"), "1"
+            ),
+            I18N.messages.getString("ExecutorController.Executor.Status.Rejected"))
     }
 
     @Test
     fun runningTestOnCorrectBracketSequenceRecognizer() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.correctBracketSeqRecognizer"), "()[]{}")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Accepted")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.correctBracketSeqRecognizer"), "()[]{}"
+            ),
+            I18N.messages.getString("ExecutorController.Executor.Status.Accepted"))
     }
 
     @Test
     fun runningTestOnTuringMachine() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.binaryNumberAdder"), "100+111")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Accepted")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.binaryNumberAdder"), "100+111"
+            ),
+            I18N.messages.getString("ExecutorController.Executor.Status.Accepted"))
     }
 
     @Test
     fun runningTestOnEvenPalindromesRecognizer() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.evenPalindromesRecognizer"), "110011")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Accepted")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.evenPalindromesRecognizer"), "110011"
+            ), I18N.messages.getString("ExecutorController.Executor.Status.Accepted"))
     }
 
     @Test
     fun runningTestOnRegisterAutomaton() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.threeZerosAndOneOne"), "110011")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Rejected")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.threeZerosAndOneOne"), "110011"
+            ), I18N.messages.getString("ExecutorController.Executor.Status.Rejected"))
     }
 
     @Test
     fun runningTestOnMealyMooreMachine() {
-        openExampleAndRunTest(I18N.automatonExamples.getString(
-            "ExamplesFragment.zeroRemover"), "110011")
-        verifyThat(".table-view", hasTableCell(I18N.messages.getString(
-            "ExecutorController.Executor.Status.Accepted")))
+        assertEquals(
+            openExampleAndRunTest(
+                I18N.automatonExamples.getString("ExamplesFragment.zeroRemover"), "110011"
+            ), I18N.messages.getString("ExecutorController.Executor.Status.Accepted"))
     }
 }
